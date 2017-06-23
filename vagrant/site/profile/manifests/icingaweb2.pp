679c7db032ec47c75357322fe346a6686c9fc3d9
Markus Frosch <markus@lazyfrosch.de>
2017-06-23 13:14:28 +0200

    icingaweb2: Enable doc and add Icinga 2 docs

--- vagrant/site/profile/manifests/icingaweb2.pp
+++ vagrant/site/profile/manifests/icingaweb2.pp
@@ -16,4 +16,29 @@ class profile::icingaweb2 {
   }
 
   create_resources('icingaweb2::config::resource_database', hiera_hash('icingaweb2::config::resource_database', {}))
+
+  # ensure doc module
+  #contain ::icingaweb2::mod::doc
+  file { '/etc/icingaweb2/enabledModules/doc':
+    ensure => link,
+    target =>  '/usr/share/icingaweb2/modules/doc',
+  }
+
+  # icinga 2 docs
+  exec { 'gunzip icinga2 docs':
+    command => 'gunzip *.md.gz',
+    path    => $::path,
+    cwd     => '/usr/share/doc/icinga2/markdown',
+  }
+
+  file {
+    '/etc/icingaweb2/enabledModules/icinga2':
+      ensure => link,
+      target =>  '/usr/share/icingaweb2/modules/icinga2';
+    '/usr/share/icingaweb2/modules/icinga2':
+      ensure => directory;
+    '/usr/share/icingaweb2/modules/icinga2/doc':
+      ensure => link,
+      target => '/usr/share/doc/icinga2/markdown';
+  }
 }
